d2b717a76c4d63823bd7dd7d8ab3fd00605919b5
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2020-10-06 14:37:31 +0200

    Placeholder stuff

---
+++ Juiceness 2020/Assets/Scripts/Mole_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole_Script : MonoBehaviour
+{
+    [SerializeField]
+    GameObject moleObject = null; 
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            SpawnMole(0,0);
+        }
+    }
+
+    public void SpawnMole(int xPosition, int yPosition){
+        Instantiate(moleObject,new Vector3(xPosition,yPosition,0),Quaternion.identity);
+    }
+}
