9f51bf6f18827f2873a53dba19f2479abd9b22b1
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2020-10-07 14:14:44 +0200

    Timer för spawning

--- Juiceness 2020/Assets/Scripts/Mole_Script.cs
+++ Juiceness 2020/Assets/Scripts/Mole_Script.cs
@@ -16,13 +16,19 @@ public class Mole_Script : MonoBehaviour
 
     
     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.A)){
+    {   
+        float timeBetweenSpawn = 1f;
+        timeBetweenSpawn -= 10*Time.deltaTime;
+        Debug.Log(timeBetweenSpawn);
+
+        if(timeBetweenSpawn <= 0f){
             SpawnMole(Random.Range(1,4),Random.Range(1,4));
+            timeBetweenSpawn = 0f;
         }
+
     }
 
-    public void SpawnMole(int xPosition, int yPosition){
+    public void SpawnMole(int xPosition, int yPosition){ //Method som sköter spawning av moles //Kalle 
         Instantiate(moleObject,new Vector3(xPosition,yPosition,0),Quaternion.identity);
     }
 }
