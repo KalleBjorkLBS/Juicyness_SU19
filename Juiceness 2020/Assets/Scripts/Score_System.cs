548d3aad650850a57da31b382052de28d79b3f8d
8dq <dq8@github.com>
2020-10-20 14:18:03 +0200

    Highscore

--- Juiceness 2020/Assets/Scripts/Score_System.cs
+++ Juiceness 2020/Assets/Scripts/Score_System.cs
@@ -7,16 +7,33 @@ public class Score_System : MonoBehaviour
 {
     [SerializeField]
     public Text scoreText = null;
-    public Text highScoreText = null;
+    public Text highscoreText = null;
+
+    string highScoreKey = "HighScore";
 
     public int gameScore = 0;
     public int highScore = 0;
-    public int whackPoints = 50;
-    
+    public int whackPoints = 10;
 
+    public void Start()
+    {
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+        highscoreText.text = highScore.ToString();
+    }
     public void MolePoints()
     {
         scoreText.text = gameScore.ToString();
         gameScore = gameScore + whackPoints;
     }
+    void OnDisable()
+    {
+
+        //If our scoree is greter than highscore, set new higscore and save.
+        if (gameScore > highScore)
+        {
+            PlayerPrefs.SetInt(highScoreKey, gameScore);
+            PlayerPrefs.Save();
+        }
+    }
+
 }
